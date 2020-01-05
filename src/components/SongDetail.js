34e122e3337dfab5d3c5f7275cf14d7742770c60
iulianstave <iulian.staverescu@gmail.com>
2020-01-05 18:11:15 +0200

    Check if a song was selected

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux';
 
 const SongDetail = (props) => {
   console.log(props);
-  return <div>SongDetail</div>;
+  if (!props.mySong) {
+    return <div>Select a song</div>
+  }
+  else {
+    return <div>SongDetail</div>;
+  }
+  
 };
 
 // wrap the SongDetail with the connect component so that
 // we get information from the redux store
 // we take selectedSong from the reducers combineReducers
 const mapStateToProps = (state) => {
-  return {song: state.selectedSong}
+  return {mySong: state.selectedSong}
 }
 
 export default connect(mapStateToProps)(SongDetail);
