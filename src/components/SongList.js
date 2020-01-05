40c8989e969b2b0bee1e80ef9511b44dc050b9df
iulianstave <iulian.staverescu@gmail.com>
2020-01-05 17:44:01 +0200

    Call action creator selectSong from SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,10 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -21,7 +25,7 @@ class SongList extends Component {
   
   
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 
@@ -30,9 +34,12 @@ class SongList extends Component {
 // state object = all the data inside our store
 // getMyState function called mapStateToProps = (state)
 const mapStateToProps = (state) => {
-  // console.log(state);
-  //  the this.props === {songs: state.songs }
+  console.log(state);
+  // this.props === {songs: state.songs }
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong: selectSong }
+  )(SongList);
